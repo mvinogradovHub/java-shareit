dc4182777954232caf3f1e6834a2ef99cf5e47e0
mvinogradovHub <mvinogradov.gee@yandex.ru>
2023-05-02 11:56:22 +0300

    fixed style

--- src/main/java/ru/practicum/shareit/request/dto/RequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/dto/RequestMapper.java
@@ -2,10 +2,10 @@ package ru.practicum.shareit.request.dto;
 
 import ru.practicum.shareit.request.ItemRequest;
 
-        /*методы и сами классы ItemRequest и ItemRequestDto доработаю в последующих спринтах т.к. в этом не требовалось,
-        но мне нужен RequestMapper чтобы использовать ItemRequestDto при сборке Item*/
+/*методы и сами классы ItemRequest и ItemRequestDto доработаю в последующих спринтах т.к. в этом не требовалось,
+но мне нужен RequestMapper чтобы использовать ItemRequestDto при сборке Item*/
 public class RequestMapper {
-    public static ItemRequestDto toItemRequestDto (ItemRequest itemRequest) {
+    public static ItemRequestDto toItemRequestDto(ItemRequest itemRequest) {
         if (itemRequest == null) {
             return null;
         }
@@ -14,7 +14,7 @@ public class RequestMapper {
                 .build();
     }
 
-    public static ItemRequest toItemRequest (ItemRequestDto itemRequestDto) {
+    public static ItemRequest toItemRequest(ItemRequestDto itemRequestDto) {
         if (itemRequestDto == null) {
             return null;
         }
